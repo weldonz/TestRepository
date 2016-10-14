cd77d9ceba3756f5f11da807d17b3ed0df83fb11
weldon <344670342@qq.com>
2016-10-14 14:21:03 +0800

    在Egit插件下尝试修改分支上的文件提交

--- src/test1/TestAddToMaster.java
+++ src/test1/TestAddToMaster.java
@@ -1,5 +1,16 @@
 package test1;
 
 public class TestAddToMaster {
+	private int num;
 
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	//尝试提交修改
+	
 }
