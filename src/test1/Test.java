1d8690461166efeb4e0246248bede5b54786dbfb
weldon <344670342@qq.com>
2016-10-14 09:09:47 +0800

    新建一个测试项目

---
+++ src/test1/Test.java
@@ -0,0 +1,17 @@
+package test1;
+
+public class Test {
+	public static void main(String[] args) {
+		int i = 1;
+		//long l = 2;
+		
+		double d = 1.0;
+		
+		String str = "1";
+		
+		boolean bl;
+		
+		
+		
+	}
+}
---
+++ src/test1/TestClass.java
@@ -0,0 +1,26 @@
+package test1;
+
+public class TestClass {
+	
+	
+	public double add(double f,double l){
+		return f+l;
+	}
+	
+	public double add(int f,int l){
+		return f+l;
+	}
+	
+	
+	public double mint(double f,double l){
+		return f-l;
+	}
+	
+	public double cheng(double f,double l){
+		return f*l;
+	}
+	
+	public double chu(double f,double l){
+		return f/l;
+	}
+}
